58b3afaca30322971c8c2fea6149f57ade3bfde1
nyurasheva <n.yurasheva@gmail.com>
2022-11-08 00:17:57 +0300

    feat: add task 6

---
+++ 06-build-page/index.js
@@ -0,0 +1,93 @@
+const fs = require("fs");
+const path = require("path");
+const { readdir, mkdir } = require('fs/promises');
+const newDir = path.join(__dirname, 'project-dist');
+const compHtml = path.join(__dirname, 'components');
+const stylePath = path.join(__dirname, 'styles');
+const stylePathOut = path.join(__dirname, 'project-dist', 'style.css');
+
+fs.rm(newDir, { recursive: true, force: true }, () => {
+  mkdir(newDir, { recursive: true }).then(() => {
+    copyDirectory(path.join(__dirname, 'assets'), path.join(newDir, 'assets'), function(err) {
+      if (err) throw err;
+    });
+    mergeStyles(path.join(newDir, 'style.css'));
+    buildHtml(path.join(__dirname, 'template.html'), path.join(newDir, 'index.html'));
+  });
+});
+
+function copyDirectory(assets, assetsDist) {
+  readdir(assets, {withFileTypes: true }).then((files) => {
+    files.forEach(file => {
+      if (file.isDirectory()) {
+        let assetsChild = path.join(assets, file.name);
+        let assetsDistChild = path.join(assetsDist, file.name);
+        copyDirectory(assetsChild, assetsDistChild);
+      } else {
+        fs.mkdir(assetsDist, { recursive: true }, function(err) {
+          if (err) throw err;
+        });
+        fs.copyFile(
+          path.join(assets, file.name),
+          path.join(assetsDist, file.name), 
+          function (err) {
+            if (err) throw err;
+          }
+        );
+      }
+    });
+  }); 
+}
+
+function mergeStyles() {
+  fs.readdir(stylePath, { withFileTypes: true }, function(err, fileNames) {
+    if (err) throw err;
+    const writeStream = fs.createWriteStream(stylePathOut);
+    fileNames.forEach(function(fileName) {
+      const ext = path.parse(fileName.name).ext;
+      if (fileName.isFile() === true && ext === '.css')  {
+        const readStream = fs.createReadStream(path.join(stylePath, fileName.name));
+        readStream.on('data', data => writeStream.write(data));
+        readStream.on('error', error => console.log('Error', error.message));
+      }
+    });
+  });  
+}
+
+function buildHtml(template, index) {
+  let html = '';
+  let templateReadStream = fs.createReadStream(template, {encoding: 'utf8'});
+  
+  templateReadStream.on('data', chunk => {
+    html = chunk.toString();
+  });
+
+  templateReadStream.on('end', () => {
+    addContent(html, index);
+  });
+}
+
+function addContent(html, index) {
+  let objHtml = {};
+  let count = 0;
+  readdir(compHtml).then((files) => {
+    files.forEach(file => {
+      let pathFile = path.join(compHtml, file);
+      let pathFileCont = file.replace(path.extname(file), '');
+      objHtml[pathFileCont] = '';
+      fs.createReadStream(
+        path.join(pathFile))
+        .on('data', (a) => {objHtml[pathFileCont] += a.toString();})
+        .on('end', () => {
+          count++;
+          if (count >= files.length) {
+            for (let i in objHtml) {
+              html = html.replace('{{'+ i + '}}', objHtml[i]);
+            }
+            let htmlStream = fs.createWriteStream(index, {encoding: 'utf8'});
+            htmlStream.write(html);
+          }
+        });
+    });
+  });
+}
